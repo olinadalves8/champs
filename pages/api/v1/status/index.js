a1c28f05545dd2485dd20ddfaac3d78ce4e29514
Danilo <olinad.alves8@gmail.com>
2026-02-20 20:13:05 -0300

    adicionando conexão máxima e versão do banco no endpoint status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,24 @@
 import database from "infra/database.js"
+import { version } from "react";
 
 async function status(request, response) {
-  const result = await database.query('SELECT 1 + 1 AS SUM;');
-  console.log (result.rows);
-  response.status(200).json({ chave: "valor" });
+const updatedAt = new Date().toISOString();
+
+const databaseVersionResult = await database.query("SHOW server_version;")
+const databaseVersionValue = (databaseVersionResult.rows[0].server_version);
+
+const databaseMaxConnectionsResult = await database.query("SHOW max_connections");
+const databaseMaxConnectionsValue = databaseMaxConnectionsResult.rows[0].max_connections;
+
+response.status(200).json({
+  updated_at: updatedAt,
+  dependencies: {
+    database: {
+      version: databaseVersionValue,
+      max_connections: parseInt(databaseMaxConnectionsValue)
+    },
+  },
+});
 }
 
 export default status;
